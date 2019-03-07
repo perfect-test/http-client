1bd070daaf2a428be64ea876b7131e4621886d6a
Mikhail Sidelnikov <m.sidelnikov@propellerads.net>
2019-03-07 16:31:00 +0300

    added artifacts

--- src/test/java/com/perfecttest/libs/example/ExampleTest.java
+++ src/test/java/com/perfecttest/libs/example/ExampleTest.java
@@ -1,19 +1,21 @@
 package com.perfecttest.libs.example;
 
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ExampleTest {
 
 
     @Test
+    @Disabled("Just for example")
     public void testMe(){
         new ExampleRequest()
                 .withId("1")
                 .sendRequest()
                 .check()
                 .checkCode(HttpStatus.SC_OK)
-                .checkBodyContains("some string")
+                .checkBodyContains("DOCTYPE")
                 .checkBodyObject(new ExampleResponseAsObject());
     }
 }
